3957da3f126189e1d5095e159c6cd3ba27fee7a1
saoirse byrne <saoirsebyrne98@gmail.com>
2024-08-07 11:46:34 +0930

    saving

---
+++ core/static/js/script.js
@@ -0,0 +1,4 @@
+$(document).on("click", ".js-toggle-modal", function(e) {
+    e.preventDefault()
+    console.log("hello i was clicked")
+})
--- eventmanager/settings.py
+++ eventmanager/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,9 +117,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
